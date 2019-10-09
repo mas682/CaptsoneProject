8f9345208d34f3d99b3d078c6e9a5575ec672450
Matthew Stropkey <mas682@pitt.edu>
2019-10-09 01:19:33 -0400

    working on database stuff, kind of confusing

--- web/models.py
+++ web/models.py
@@ -17,8 +17,42 @@ def initdb_command():
 
 	print('Initialized the database.')
 
+class Tags(db.Model):
+	__tablename__ = 'tags'
+	tid = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(24), primary_key = True)
+	projects = relaionship('projects', secondary = 'link')
+
+class Applicants(db.Model):
+	__tablename__ = 'applicants'
+	email = db.Column(db.String(30), primary_key = True)
+	password = db.Column(db.String(20), nullable = False)
+	major = db.Column(db.String(20))
+
+class Link(db.Model):
+	__tablename__ = 'link'
+	tid = db.Column(db.String(24), ForeignKey('tags.name'), primary_key = True)
+	pid = db.Column(db.Integer, ForeignKey('projects.pid'), primary_key = True)
+
+
+class Providers(db.Model):
+	__tablename__ = 'providers'
+	prov_id = db.Column(db.Integer, primary_key = True)
+	email = db.Column(db.String(30), primary_key = True)
+	password = db.Column(db.String(20), nullable = False)
+	company = db.Column(db.String(40))
+
+class Projects(db.Model):
+	__tablename__ = 'projects'
+	pid = db.Column(db.Integer, primary_key = True)
+	P_name = db.Column(db.String(100), nullable = False)
+	description = db.Column(db.String(400), nullable = False)
+	provider = relationship("Provider", backref="projects")
+	p_tags = relationship('tags', secondary = 'link')
+
 
 class User(db.Model):
+	__tablename__ = 'users'
 	user_id   = db.Column(db.Integer,    primary_key = True)
 	username  = db.Column(db.String(24), nullable = False)
 	email     = db.Column(db.String(80), nullable = False)
